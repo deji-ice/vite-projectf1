82dfb904afc5750bd6f43bcb9fa467a19e2c41cf
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-01-25 02:02:30 +0100

    added gsap

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,29 @@ import NavBar from "./components/NavBar";
 import Greeting from "./components/Greeting";
 import CardList from "./components/CardList";
 import { ActionLink } from "./components/ActionLink";
+import Modal from "./components/Modal";
+import { useState } from "react";
 
 const App = () => {
-  const logger = ()=>{
-    console.log("second")
-  }
+  const [show, setShow] = useState(false);
   return (
-    <div className="bg-yellow-500">
-      <NavBar />
-      <ActionLink/>
-      {/* <button
-        onClick={() => {
-          console.log("ghjkdfj")
-          logger()
-        }}
+    <div className=" relative h-screen bg-yellow-500">
+      <Modal show={show} setShow={setShow} content title />
+{/* 
+      <NavBar /> */}
+      <ActionLink />
+      <button
         className="bg-black text-white px-3 py-2"
+        onClick={() => setShow(true)}
       >
         click me
-      </button> */}
-      <Greeting name={"gladys"} age={"28"} />
+      </button>
+      {/* <Greeting name={"gladys"} age={"28"} />
       <Greeting>Babatunde</Greeting>
       <Greeting username="wahab" />
       <CardList />
       <img src="/Screenshot 2024-01-15 094857.png" />
-      <img width={"200px"} src={image} />
+      <img width={"200px"} src={image} /> */}
     </div>
   );
 };
